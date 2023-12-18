69de0a16740ad60361eb16442465eee78583779b
邓玄 <dengxuan@dengxuandeMacBook-Pro-4.local>
2023-12-18 16:43:37 +0800

    feat: 修改

--- src/markdown/index.ts
+++ src/markdown/index.ts
@@ -15,13 +15,5 @@ const dir: {
     cover:
       "https://assets-us-01.kc-usercontent.com:443/a99c6ff3-1e1d-000a-2573-c93dfb21cf6e/5900325b-5bee-44dc-828f-988078a63472/cover.jpg",
   },
-  "ts/base2": {
-    path: "ts/base",
-    title: "TypeScript相关",
-    create_time: "2023年12月15日 15:12",
-    des: "TypeScript基础语法",
-    cover:
-      "https://assets-us-01.kc-usercontent.com:443/a99c6ff3-1e1d-000a-2573-c93dfb21cf6e/5900325b-5bee-44dc-828f-988078a63472/cover.jpg",
-  },
 };
 export default dir;
